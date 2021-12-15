36eb1196306044c5b07f281863637816ab71baea
pjonasson <88997326+pjonasson@users.noreply.github.com>
2021-12-14 18:07:17 -0600

    Created a show function based on user params for name

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,4 +3,15 @@ class StocksController < ApplicationController
     stocks = Stock.all
     render json: stocks
   end
+
+  def show
+    stocks = []
+    nasdaq = Stock.all
+    nasdaq.each do |stock|
+      if stock.name.downcase.include?(params[:name])
+        stocks << stock
+      end
+    end
+    render json: stocks
+  end
 end
